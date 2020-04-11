bb16eb27ddd8520fea39fbbf6e7960ee3d895e99
aapxx87 <57785764+aapxx87@users.noreply.github.com>
2020-04-11 18:06:30 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,42 @@
+var main = function() {
+
+  $.getJSON("cards/aceOfSpades.json", function(card) {
+
+    // создаем элемент для хранения карты
+    var $cardParagraph = $("<p>");
+
+    // создаем
+    $cardParagraph.text(card.rank);
+    $cardParagraph.text(card.suit);
+
+    // связываем абзац с картой с main
+    $("main").append($cardParagraph);
+
+  });
+
+
+
+
+  // $.getJSON("cards/hand.json", function(hand) {
+  //
+  //   var $list = $("<ul>");
+  //
+  //   // hand — массив, поэтому мы можем применить к нему итерационный процесс // с помощью цикла forEach
+  //   hand.forEach(function(card) {
+  //
+  //     // создаем элемент списка для хранения карты // и присоединяем его к списку
+  //     var $card = $("<li>");
+  //
+  //     $card.text(card.rank + " of " + card.suit);
+  //
+  //     $list.append($card);
+  //
+  //   });
+  //   // присоединяем список к элементу main
+  //   $("main").append($list);
+  // });
+
+};
+
+
+$(document).ready(main);

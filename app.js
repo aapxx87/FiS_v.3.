9694cb772eb333a13e2d1a4222f40e7d5dca0d20
Anatolij Poedintsev <aapspb1987@gmail.com>
2020-04-15 22:39:08 +0300

    update style + content

--- app.js
+++ app.js
@@ -20,5 +20,25 @@ $(document).ready(function() {
     }
   );
 
+  $("#clickMe5").click(function() {
+      $("#picframe5").slideToggle();
+    }
+  );
+
+  $("#clickMe6").click(function() {
+      $("#picframe6").slideToggle();
+    }
+  );
+
+  $("#clickMe7").click(function() {
+      $("#picframe7").slideToggle();
+    }
+  );
+
+  $("#clickMe8").click(function() {
+      $("#picframe8").slideToggle();
+    }
+  );
+
 
 });

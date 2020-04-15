3a9164900790d216f6e04e3bf2c6abe27a4516fe
aapxx87 <57785764+aapxx87@users.noreply.github.com>
2020-04-15 21:37:50 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+
+  $("#clickMe").click(function() {
+
+      $("#picframe").slideToggle();
+
+    }
+
+  );
+
+
+  $("#clickMe2").click(function() {
+
+      $("#picframe2").slideToggle();
+
+    }
+
+  );
+
+
+  $("#clickMe3").click(function() {
+
+      $("#picframe3").slideToggle();
+
+    }
+
+  );
+
+
+  $("#clickMe4").click(function() {
+
+      $("#picframe4").slideToggle();
+
+    }
+
+  );
+
+
+});

95e0e574def74ae4d4a5d81be2b8a121c0567da4
aapxx87 <57785764+aapxx87@users.noreply.github.com>
2020-04-15 21:47:39 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,23 @@
 $(document).ready(function() {
 
   $("#clickMe").click(function() {
-
       $("#picframe").slideToggle();
-
     }
-
   );
-
 
   $("#clickMe2").click(function() {
-
       $("#picframe2").slideToggle();
-
     }
-
   );
-
 
   $("#clickMe3").click(function() {
-
       $("#picframe3").slideToggle();
-
     }
-
   );
 
-
   $("#clickMe4").click(function() {
-
       $("#picframe4").slideToggle();
-
     }
-
   );
 
 
